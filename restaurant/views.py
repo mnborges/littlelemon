9c761ad4fe53916e5f6095486fd3b433dfaf4dfd
Maieza Borges <maieza.borges@gmail.com>
2023-08-19 17:54:49 -0400

    feat: setting up the static routes and assets

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,5 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view()
 def index(request):
-    return Response("OK", status=status.HTTP_200_OK)
+    return render(request, 'index.html', {})
