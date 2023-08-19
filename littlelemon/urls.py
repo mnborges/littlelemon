32424ca71f21a35cb00d49f0487a3f056f4dd5fb
Maieza Borges <maieza.borges@gmail.com>
2023-08-19 15:26:00 -0400

    refactor: changed app's name from 'reservations' to 'restaurant'

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('reservation.urls')),
+    path('api/', include('restaurant.urls')),
     path('admin/', admin.site.urls),
 ]
