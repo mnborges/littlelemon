4a07d31da5291748edb0d311740aedbb8a99509b
Maieza Borges <maieza.borges@gmail.com>
2023-08-20 14:05:07 -0400

    feat: set up the menu api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+    
+class MenuItemView(generics.ListCreateAPIView):
+    queryset= MenuItem.objects.all()
+    serializer_class= MenuItemSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= MenuItem.objects.all()
+    serializer_class= MenuItemSerializer
