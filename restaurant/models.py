075f27f8c9b4c618b30af0dc852d539ba8df7bc4
Maieza Borges <maieza.borges@gmail.com>
2023-08-20 12:58:53 -0400

    feat: setting up the models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
+    
+    def __str__(self) -> str:
+        return f"({self.id}) - {self.name}"
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return f"({self.id}) - {self.title}"
