c03663b014da7c492d5afff6892c911e5ad7653b
Maieza Borges <maieza.borges@gmail.com>
2023-08-28 20:05:48 -0400

    test(MenuViewTest): add tests to cover all success scenarios to the menu api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('items/', views.MenuItemView.as_view(), name='menu-items'),
-    path('items/<int:pk>/', views.SingleMenuItemView.as_view())
+    path('items/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-single-item')
 ]
